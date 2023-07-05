101c8a8ff0aac6a4283f6715ccbaa43d1645b5a8
Mukul <mukultotla4@gmail.com>
2023-07-06 01:35:27 +0530

    implement Polymorphism

---
+++ polymorphism.ts
@@ -0,0 +1,27 @@
+// Dynamic Polymorphism - Method Overriding
+class Animal {
+  printAnimal(): void {
+    console.log("Calling from Animal class");
+  }
+  printAnimalTwo(): void {
+    console.log("Calling again from Animal class");
+  }
+}
+
+class Tiger extends Animal {
+  // method override
+  printAnimal(): void {
+    console.log("Calling from Tiger class");
+  }
+}
+
+let animal = new Animal();
+animal.printAnimal();
+animal.printAnimalTwo();
+
+let tiger = new Tiger();
+animal = tiger;
+animal.printAnimal();
+
+
+// Static polymorphism - Method Overloading
